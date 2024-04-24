26d31c325eb0b23bdd0e6ae70cc8933df449d7b6
j.riquelmee <101286474+DonTaka@users.noreply.github.com>
2024-04-24 16:39:05 -0400

    Upload Actividad Jquery

--- js/func.js
+++ js/func.js
@@ -1,19 +1,55 @@
-function verInfo() {
-    var user = document.getElementById("user").value;
-    var pass = document.getElementById("pass").value;
-    console.log("User: " + user + " Pass:" + pass);
+$(document).ready(function () {
 
-    if (user == 'user1' && pass == 'pass1234') {
-        document.getElementById("resultado").innerHTML = "<div class='alert alert-success col-4 mx-auto h-50'>" +
-            "<p>Conexion Exitosa</p></div>";
 
-    } else {
-        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger col-4 mx-auto h-50'>" +
-            "<p>Usuario y/o contraseña incorrecto</p></div>";
+    $("#btnEnviar").click(function () {
+        $("#formulario").submit();
+    })
+
+    $("#btnGenerar").click(function () {
+        let rut = $("#txtRut").val();
+        let nombre = $("#txtNombre").val();
+        let appaterno = $("#txtAppaterno").val();
+        let apmaterno = $("#txtApmaterno").val();
+        let fecha = $("#txtFecha").val();
+        let edad = $("#txtEdad").val();
+        let genero = $("input[name=optGenero]:checked").val();
+        let mail = $("#txtMail").val();
+        let telefono = $("#txtTelefono").val();
+        let profesion = $("#txtProf").val();
+        let comentario = $("#txtComentario").val();
+        console.log(genero)
+        validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, telefono)
+    })
+
+    function validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, celular) {
+
+        if (String(rut).length < 9 || String(rut).length > 10) {
+            /* Validacion Rut */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Rut debe tener largo entre 9 y 10 caracteres</div>");
+        } else if (String(nombre).length < 3 || String(nombre).length > 20) {
+            /* Validacion Nombre */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Nombre debe tener largo entre 3 y 20 caracteres</div>");
+        } else if (String(appaterno).length <= 3 || String(appaterno).length >= 20) {
+            /* Validacion Apellido Paterno */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Paterno debe tener largo entre 3 y 20 caracteres</div>");
+        } else if (String(apmaterno).length <= 3 || String(apmaterno).length >= 20) {
+            /* Validacion Apellido Materno */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Materno debe tener largo entre 3 y 20 caracteres</div>");
+        } else if (Number(edad) < 18 && Number(edad) > 30) {
+            /* Validacion Edad */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Edad debe estar entre 18 y 30 años</div>");
+        } else if (genero != "Masculino" && genero != "Femenino" && genero != "Otro") {
+            /* Validacion Genero */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Debe seleccionar un genero valido</div>");
+
+        } else if (String(celular).length < 9 || String(celular).length > 12) {
+            /* Validacion Edad */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Telefono debe tener entre 9 y 12 digitos</div>");
+        } else {
+            /* Confirmacion */
+            $("#check").html("<div class='alert alert-success w-50 mx-auto text-center' >Datos Ingresado con exito</div>");
+
+        }
+
     }
-
-}
-
-function clearButton() {
-    document.getElementById("resultado").innerHTML = "";
-}
+});
--- js/funciones.js
+++ js/funciones.js
@@ -1,33 +1,39 @@
 function validar() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("pass").value;
     /* 
-        username: min 5 max 30 
-        password: largo min 8 
+        username: min 5 max 20
+        password: min 8    
     */
-
-    const user = document.getElementById("username").value;
-    const pass = document.getElementById("pass").value;
-    if (String(user).length >= 5 && String(user).length <= 30) {
-        var res = document.getElementById("username");
-        res.style.border = "1px solid green"
+    if (String(user).length >= 5 && String(user).length <= 20) {
         if (String(pass).length >= 8) {
-            /* Correcto */
-            var res = document.getElementById("pass");
-            res.style.border = "1px solid green"
+            /* Funciono */
+            document.getElementById("username").style.border = "1px solid lightgrey";
+            document.getElementById("pass").style.border = "1px solid lightgrey";
+            /* Alerta */
             document.getElementById("resultado").innerHTML = "<div class='alert alert-success w-50 mx-auto text-center'>" +
                 "Acceso Concedido</div>"
         } else {
-            /* Error pass */
-
-            var res = document.getElementById("pass");
-            res.style.border = "1px solid red"
+            /* Pass error */
+            /* Borde */
+            document.getElementById("pass").style.border = "1px solid red";
+            /* Alerta */
             document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>" +
                 "Contraseña debe tener minimo 8 caracteres</div>"
+
         }
     } else {
-        /* Error User */
-        var res = document.getElementById("username");
-        res.style.border = "1px solid red"
-        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>" +
-            "Usuario debe tener minimo 5 y maximo 30 caracteres</div>";
+        /* User Error */
+        /* 
+            1.- Alerta
+            2.- Label
+            3.- animaciones
+            4.- letras rojo
+            5.- borde en rojo 
+        */
+        /* Borde */
+        document.getElementById("username").style.border = "1px solid red";
+        /* Alerta */
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Usuario debe tener minimo 5 y maximo 20 caracteres</div>"
     }
 }
